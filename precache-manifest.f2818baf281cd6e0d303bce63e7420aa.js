3d74573cbc5a88343cf0b30be27bdf2b3b14143e
Aj <jayanwana@gmail.com>
2020-05-18 21:51:09 +0100

    Updates

---
+++ precache-manifest.f2818baf281cd6e0d303bce63e7420aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6d693115a2f09038a2847f3dbe7cbd",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "ea9c992dce8d00304847",
+    "url": "/calc/static/css/main.604ad8e5.chunk.css"
+  },
+  {
+    "revision": "db284d1dafce010da501",
+    "url": "/calc/static/js/2.47da3053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc/static/js/2.47da3053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9c992dce8d00304847",
+    "url": "/calc/static/js/main.43350a44.chunk.js"
+  },
+  {
+    "revision": "2d8a876cdc3e1f359541",
+    "url": "/calc/static/js/runtime-main.367f0b21.js"
+  }
+]);
